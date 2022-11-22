c29eea08a1f7e1d73c9c2ed1d0f19b55a1499c60
Museyib <museyib.huseynov.dev@gmail.com>
2022-11-22 22:23:51 +0400

    final version

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import './index.css'
-import Root, {loader as rootLoader, action as rootAction} from './routes/root'
+import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
-import Contact, {loader as contactLoader} from './routes/contact'
-import EditContact, {action as editAction} from './routes/edit'
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact'
+import EditContact, { action as editAction } from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
+import Index from './routes/index'
 
 const router = createBrowserRouter([
   {
@@ -16,22 +21,32 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
       },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      }
-    ]
+    ],
   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
